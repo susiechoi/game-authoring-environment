5e44ada5bdaf6ca271f8cb6e0011ab341cd53abe
Ben Hodgson <bmh43@duke.edu>
2018-03-27 15:12:03 -0400

    Created controller interface and ChiefController class in a new controller package. Also created the AuthoringView class responsible for displaying screens specific to the authoring environment

---
+++ src/authoring/AuthoringView.java
@@ -0,0 +1,15 @@
+package authoring;
+
+import screen.Screen;
+
+/**
+ * 
+ * @author Ben Hodgson 3/27/18
+ *
+ * Class that implements the Screen interface to display authoring specific screens 
+ * in the user interface
+ */
+
+public class AuthoringView implements Screen {
+
+}
---
+++ src/file/FileIO.java
@@ -0,0 +1,13 @@
+package file;
+
+/**
+ * 
+ * @author Ben Hodgson 3/27/18
+ *
+ * Interface used for accessing files within the program file system. 
+ */
+
+public interface FileIO {
+    
+
+}
