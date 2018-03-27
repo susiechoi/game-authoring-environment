a06bd8668e4d0e414b521ce176be9ba35cd27864
Susie Choi <sc477@duke.edu>
2018-03-27 17:25:44 -0400

    new dropdown menu object, added to authoring view

---
+++ doc/usecases/AuthoringView.java
@@ -0,0 +1,29 @@
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+
+class AuthoringView {
+
+	private FileIO myFileIO; 
+
+	protected AuthoringView(FileIO fileIO) {
+		myFileIO = fileIO; 
+	}
+
+	private void populateOptions(String dropdownPurpose, List<String> options) {
+		ObservableList<String> optionsAsObservableList = FXCollections.observableArrayList();
+		for (String option : options) {
+			optionsAsObservableList.add(option);
+		}
+		DropdownMenu menuToPopulate = new DropdownMenu(dropdownPurpose, new ComboBox<String>()); 
+		menuToPopulate.setItems(optionsAsObservableList);
+		menuToPopulate.selectionReceived().addListener((arg0, arg1, arg2)->{
+			String selection = menuToPopulate.getSelection((int) arg2);
+			// must pass menuToPopulate.getPurpose and selection to AuthoringController to influence Model (temp view) 
+		});
+	}
+
+
+}
---
+++ doc/usecases/DropdownMenu.java
@@ -0,0 +1,36 @@
+import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.SingleSelectionModel;
+
+public class DropdownMenu {
+
+	private String myPurpose; // can also represent this with polymorphism
+	private ComboBox<String> myComboBox;
+	
+	public DropdownMenu(String purpose, ComboBox<String> comboBox) {
+		myPurpose = purpose; 
+		myComboBox = comboBox; 
+	}
+	
+	public String getPurpose() {
+		return myPurpose;
+	}
+	
+	public void setItems(ObservableList<String> items) {
+		myComboBox.setItems(items);
+	}
+	
+	public ReadOnlyIntegerProperty selectionReceived() {
+		return myComboBox.getSelectionModel().selectedIndexProperty(); 
+	}
+	
+	public String getSelection(int index) {
+		return myComboBox.getItems().get(index);
+	}
+	
+	public ObservableList<String> getItems() {
+		return myComboBox.getItems();
+	}
+	
+}
