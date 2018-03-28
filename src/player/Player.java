c97f7625f878026744c50fe155493f8eb3b7c9a8
Ben Hodgson <bmh43@duke.edu>
2018-03-28 15:53:42 -0400

    Added Player, Tower, and Ability interfaces in new corresponding packages

---
+++ src/player/Player.java
@@ -0,0 +1,12 @@
+package player;
+
+/**
+ * 
+ * @author Ben Hodgson 3/28/18
+ * 
+ * Interface for a game player
+ */
+
+public interface Player {
+
+}
---
+++ src/towers/Tower.java
@@ -0,0 +1,12 @@
+package towers;
+
+/**
+ * 
+ * @author Ben Hodgson 3/28/18
+ *
+ * Interface for Tower objects in the game. 
+ */
+
+public interface Tower {
+
+}
