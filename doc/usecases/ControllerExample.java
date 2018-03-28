7c3fecc567146267df88865637a3c2b347037c03
Susie Choi <sc477@duke.edu>
2018-03-28 10:00:21 -0400

    Addedauthoring tower

---
+++ doc/usecases/ControllerExample.java
@@ -0,0 +1,12 @@
+package usecases;
+
+/**
+ * 
+ * @author Ben Hodgson 3/27/18
+ *
+ * Interface for program Controller objects
+ */
+
+public interface ControllerExample {
+
+}
---
+++ src/authoring/TowerView.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Object to represent (model) towers
+ * @author Susie Choi 
+ * 
+ */
+
+package authoring;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+class TowerView {
+	
+	protected TowerView(Image image, double size, double x, double y, 
+							double effect, double range, double speed, double cost, 
+							TowerView upgrade) {
+		
+	}
+	
+	
+	/**
+	 * Changes an aspect of a tower
+	 * @param field - the aspect of the tower to be changed
+	 * @param changeToValue - the new value of the field 
+	 */
+	protected void changeTower(String field, String changeToValue) {
+		
+	}
+
+}
