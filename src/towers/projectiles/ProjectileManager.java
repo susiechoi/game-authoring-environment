c84231446dc0bef7e714a86db27c1f3fe3a6b64c
Ben Hodgson <bmh43@duke.edu>
2018-03-29 14:45:10 -0400

    Changed the ProjectileManager to implement Trigger

--- src/towers/projectiles/ProjectileManager.java
+++ src/towers/projectiles/ProjectileManager.java
@@ -1,5 +1,7 @@
 package towers.projectiles;
 
+import towers.trigger.Trigger;
+
 /**
  * 
  * @author Ben Hodgson 3/28/18
@@ -7,12 +9,18 @@ package towers.projectiles;
  * Class to manage different projectiles. Takes a defined range and trigger to determine when to
  * fire projectiles
  */
-public class ProjectileManager {
+public class ProjectileManager implements Trigger {
     
     /**
      * Sets the current projectile type managed by the ProjectileManager
      */
     public void setProjectile() {
+	
+    }
+
+    @Override
+    public void fire() {
+	// TODO Auto-generated method stub
 	
     }
 
