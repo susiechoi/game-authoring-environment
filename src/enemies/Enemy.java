922580dfdf1ccc151ca2429e3a25608b5e7983d3
Ben Hodgson <bmh43@duke.edu>
2018-03-29 16:54:23 -0400

    Added more methods to the Enemy interface

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -1,5 +1,7 @@
 package enemies;
 
+import towers.projectiles.Projectile;
+
 /**
  * 
  * @author Ben Hodgson 3/28/18
@@ -7,6 +9,18 @@ package enemies;
  * Interface for enemy functionality
  */
 public interface Enemy {
+    
+    /**
+     * Handles when the Enemy is hit by a tower
+     * 
+     * @param projectile: the projectile that hit the enemy
+     */
+    public void getHitBy(Projectile projectile);
+    
+    /**
+     * Handles updating the enemy position to follow the path
+     */
+    public void followPath();
     
     /**
      * Updates the properties of the enemy. For example, health, position, etc.
