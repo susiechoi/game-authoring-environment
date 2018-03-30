a7072abc20a328238f6513daba844d1e63940bf4
Susie Choi <sc477@duke.edu>
2018-03-30 17:09:38 -0400

    created implementations of screen interface

---
+++ src/authoring/frontend/EnemyScreen.java
@@ -0,0 +1,18 @@
+package authoring.frontend;
+
+import javafx.scene.Node;
+
+class EnemyScreen implements Screen {
+
+	@Override
+	public Node getScreen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeScreen() {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/authoring/frontend/TowerScreen.java
@@ -0,0 +1,19 @@
+package authoring.frontend;
+
+import javafx.scene.Node;
+
+class TowerScreen implements Screen {
+
+	@Override
+	public Node getScreen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void makeScreen() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
