4a2ed60397932fc7ae42812fd9bc0d8e3669eac1
Susie Choi <sc477@duke.edu>
2018-03-31 09:56:03 -0400

    addded dropdown methods to partsfactory

--- src/authoring/frontend/PartsFactory.java
+++ src/authoring/frontend/PartsFactory.java
@@ -1,23 +1,67 @@
 package authoring.frontend;
 
+import java.io.File;
 import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class PartsFactory {
 
-	public static Button makeButton(String buttonText, double xPos, double yPos, double length, double height) {
-		return null; 
+	public static Button makeButton(String buttonText, double length, double height) {
+		Button newButton = new Button(buttonText);
+		newButton.setPrefWidth(length);
+		newButton.setPrefHeight(height);
+		return newButton; 
 	}
 	
-	public static ComboBox<String> makeDropDown(List<String> dropdownOptions, double xPos, double yPos, double length, double height) {
-		return null; 
+	public static ComboBox<String> makeTextDropdown(List<String> dropdownOptions, double length, double height) {
+		 ObservableList<String> options = FXCollections.observableArrayList(dropdownOptions);
+		 ComboBox<String> newDropdown = new ComboBox<String>(options);
+		 newDropdown.setPrefWidth(length);
+		 newDropdown.setPrefHeight(height);
+		 return newDropdown; 
 	}
 	
-	public static ScrollPane makeScrollPane(List<String> fileNames, double xPos, double yPos, double length, double height) {
-		return null; 
+	public static ComboBox<Image> makeImageDropdown(List<Image> dropdownImages, double length, double height) {
+		 ObservableList<Image> imageOptions = FXCollections.observableArrayList(dropdownImages);
+		 ComboBox<Image> newDropdown = new ComboBox<Image>(imageOptions);
+		 newDropdown.setPrefWidth(length);
+		 newDropdown.setPrefHeight(height);
+		 return newDropdown; 
+	}
+	
+	public static ScrollPane makeTextScrollPane(List<String> options, double length, double height) {
+		ScrollPane newPane = new ScrollPane();
+		String allOptions = "";
+		for (String s : options) {
+			allOptions += s+"\n";
+		}
+		newPane.setContent(new Text(allOptions));
+		newPane.setPrefWidth(length);
+		newPane.setPrefHeight(height);
+		return newPane; 
+	}
+	
+	public static ScrollPane makeImageScrollPane(File[] files, double length, double height) {
+		ScrollPane newPane = new ScrollPane();
+		VBox imageConsolidator = new VBox();
+		for (File f : files) {
+			ImageView oneImageView = new ImageView();
+			oneImageView.setImage(new Image(f.toURI().toString()));
+			imageConsolidator.getChildren().add(oneImageView);
+		}
+		newPane.setContent(imageConsolidator);
+		newPane.setPrefWidth(length);
+		newPane.setPrefHeight(height);
+		return newPane; 
 	}
 	
 }
--- src/authoring/frontend/SpecifyEnemyScreen.java
+++ src/authoring/frontend/SpecifyEnemyScreen.java
@@ -1,10 +1,19 @@
 package authoring.frontend;
 
+import java.util.ArrayList;
+
+import javafx.scene.layout.VBox;
+
 public class SpecifyEnemyScreen extends SpecifyObjectScreen {
 
 	@Override
 	public void makeScreen() {
-		
+		VBox v = new VBox();
+		ArrayList<String> testingList = new ArrayList<String>();
+		testingList.add("hi");
+		testingList.add("bye");
+		v.getChildren().add(makeTextDropdown(testingList, 60, 20));
+		myScreen = v; 
 	}
 
 }
