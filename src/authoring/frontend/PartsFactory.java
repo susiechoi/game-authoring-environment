c48824a785918bd40f02289864115b7dc98b9938
Susie Choi <sc477@duke.edu>
2018-03-30 20:00:37 -0400

    added barebones for PartsFactory

---
+++ src/authoring/frontend/PartsFactory.java
@@ -0,0 +1,23 @@
+package authoring.frontend;
+
+import java.util.List;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ScrollPane;
+
+public class PartsFactory {
+
+	public Button makeButton(String buttonText, double xPos, double yPos, double length, double height) {
+		return null; 
+	}
+	
+	public ComboBox<String> makeDropDown(List<String> dropdownOptions, double xPos, double yPos, double length, double height) {
+		return null; 
+	}
+	
+	public ScrollPane makeScrollPane(List<String> fileNames, double xPos, double yPos, double length, double height) {
+		return null; 
+	}
+	
+}
