c64b077c2a48abc9d51511b6ef58eb908dd7f5f9
Sarah Bland <scb49@duke.edu>
2018-03-31 14:19:15 -0400

    added UIFactory to Screen

--- src/authoring/frontend/Screen.java
+++ src/authoring/frontend/Screen.java
@@ -4,16 +4,20 @@ import javafx.scene.Scene;
 /**
  * 
  * @author API - Ben Hodgson 
- * 
+ * @author transition to abstract class - Sarah Bland
  * Interface used for generating Screen objects, which represent the entirety of the view 
  * displayed to the user. 
  */
 
 public abstract class Screen {
 	private Scene myScreen;
+	private PartsFactory myUIFactory;
     
 	public static final String DEFAULT_SHARED_STYLESHEET = "styling/SharedStyling.css";
 	
+	public Screen() {
+		myUIFactory = new PartsFactory();
+	}
     /**
      * Creates the Screen
      */
@@ -30,5 +34,8 @@ public abstract class Screen {
     }
     protected void setScreen(Scene newScreen) {
     		myScreen = newScreen;
+    }
+    protected PartsFactory getUIFactory() {
+    		return myUIFactory;
     }
 }
