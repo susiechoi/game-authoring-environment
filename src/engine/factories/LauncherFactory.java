35997c4351da16d0c3265ecc13ac15d96a4eaa32
kmv10 <kmv10@duke.edu>
2018-04-01 21:12:10 -0400

    Added manager and factory classes

---
+++ src/engine/factories/LauncherFactory.java
@@ -0,0 +1,38 @@
+package engine.factories;
+
+import java.util.Collection;
+import java.util.Map;
+
+import engine.towers.launcher.Launcher;
+
+/**
+ * Factory class for constructing a launcher.
+ * 
+ * @author Katherine Van Dyk
+ *
+ */
+public class LauncherFactory extends Factory {
+    
+    private ProjectileFactory projectileFactory;
+    
+    /**
+     * Constructor that takes in parameter data for a launcher
+     * 
+     * @param data: Map containing launcher type as key, and appropriate parameter data as value
+     */
+    public LauncherFactory(Map<String, Collection<Object>> data) {
+	super(data);
+    }
+    
+    /**
+     * Constructs and returns a launcher object of the appropriate type
+     * 
+     * @return Launcher object of type string
+     */
+    public Launcher construct(String type) {
+	return null;
+    }
+
+    
+
+}
