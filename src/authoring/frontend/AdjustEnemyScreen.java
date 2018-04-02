f1beac9c7cac9a08591666840b1a903ae7abf4b6
Susie Choi <sc477@duke.edu>
2018-04-02 14:47:16 -0400

    removed slider compensatory positioning

--- src/authoring/frontend/AdjustEnemyScreen.java
+++ src/authoring/frontend/AdjustEnemyScreen.java
@@ -2,7 +2,6 @@ package authoring.frontend;
 
 import authoring.frontend.exceptions.MissingPropertiesException;
 import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -27,23 +26,23 @@ class AdjustEnemyScreen extends AdjustScreen {
 		
 		HBox enemyNameSelect = getUIFactory().setupPromptAndTextField("", "Enemy Name: "); 
 
-		ImageView enemyImageDisplay = new ImageView(); 
+//		ImageView enemyImageDisplay = new ImageView(); 
 		HBox enemyImageSelect = new HBox();
 		try {
-			enemyImageSelect = getUIFactory().setupImageSelector(myPropertiesReader, "Enemy ", ENEMY_IMAGES, enemyImageDisplay, 100);
+			enemyImageSelect = getUIFactory().setupImageSelector(myPropertiesReader, "Enemy ", ENEMY_IMAGES, 100);
 		} catch (MissingPropertiesException e) {
 			// TODO FIX
 		} 
 		
-		VBox enemySpeed = getUIFactory().setupPromptAndSlider("enemySpeedSlider", "Enemy Speed: ", DEFAULT_ENEMY_MAX_SPEED); 
-		VBox enemyHealthImpact = getUIFactory().setupPromptAndSlider("enemyHealthImpactSlider", "Enemy Health Impact: ", DEFAULT_ENEMY_MAX_HEALTH_IMPACT); 
-		VBox enemy$Impact = getUIFactory().setupPromptAndSlider("enemyMoneyImpactSlider", "Enemy $ Impact: ", DEFAULT_ENEMY_MAX_$_IMPACT); 
+		HBox enemySpeed = getUIFactory().setupPromptAndSlider("enemySpeedSlider", "Enemy Speed: ", DEFAULT_ENEMY_MAX_SPEED); 
+		HBox enemyHealthImpact = getUIFactory().setupPromptAndSlider("enemyHealthImpactSlider", "Enemy Health Impact: ", DEFAULT_ENEMY_MAX_HEALTH_IMPACT); 
+		HBox enemy$Impact = getUIFactory().setupPromptAndSlider("enemyMoneyImpactSlider", "Enemy $ Impact: ", DEFAULT_ENEMY_MAX_$_IMPACT); 
 		
 		HBox backAndApply = setupBackAndApplyButton(); 
 
 		vb.getChildren().add(enemyNameSelect);
 		vb.getChildren().add(enemyImageSelect);
-		vb.getChildren().add(enemyImageDisplay);
+//		vb.getChildren().add(enemyImageDisplay);
 		vb.getChildren().add(enemySpeed);
 		vb.getChildren().add(enemyHealthImpact);
 		vb.getChildren().add(enemy$Impact);
