3f83f1d2be3c302667c89630157638712a9f5a63
Susie Choi <sc477@duke.edu>
2018-04-04 13:43:52 -0400

    adding to adjustresourcesscreen

--- src/authoring/frontend/AdjustResourcesScreen.java
+++ src/authoring/frontend/AdjustResourcesScreen.java
@@ -2,6 +2,10 @@ package authoring.frontend;
 
 import authoring.frontend.exceptions.MissingPropertiesException;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class AdjustResourcesScreen extends AdjustScreen {
 
@@ -11,8 +15,20 @@ public class AdjustResourcesScreen extends AdjustScreen {
 
 	@Override
 	protected Scene makeScreenWithoutStyling() throws MissingPropertiesException {
-		// TODO Auto-generated method stub
-		return null;
+		VBox vb = new VBox(); 
+
+		Button newObjectButton = makeCreateNewObjectButton(myObjectDescription);
+		
+		HBox objectsDropdown = getUIFactory().setupPromptAndDropdown("objectOptions", "Edit Existing "+myObjectDescription, myObjectOptions); 
+		HBox backAndApplyButton = setupBackAndApplyButton();
+		
+		vb.getChildren().add(getUIFactory().makeScreenTitleText(myObjectDescription));
+		vb.getChildren().add(newObjectButton);
+		vb.getChildren().add(orText);
+		vb.getChildren().add(objectsDropdown);
+		vb.getChildren().add(backAndApplyButton);
+
+		return new Scene(vb, 1500, 900); // TODO move to properties file
 	}
 
 }
