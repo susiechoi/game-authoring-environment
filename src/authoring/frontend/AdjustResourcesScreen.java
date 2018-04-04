899b3c6974b9fe13ae5a0a5918b4e6cece554a4c
Susie Choi <sc477@duke.edu>
2018-04-04 13:41:04 -0400

    added adjust resource screen class

---
+++ src/authoring/frontend/AdjustResourcesScreen.java
@@ -0,0 +1,18 @@
+package authoring.frontend;
+
+import authoring.frontend.exceptions.MissingPropertiesException;
+import javafx.scene.Scene;
+
+public class AdjustResourcesScreen extends AdjustScreen {
+
+	protected AdjustResourcesScreen(AuthoringView view) {
+		super(view);
+	}
+
+	@Override
+	protected Scene makeScreenWithoutStyling() throws MissingPropertiesException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
