ff297bc58bd01924a43f5fcdda7b8eebdd610fc2
Miles Todzo <mt265@duke.edu>
2018-04-05 14:24:57 -0400

    PlayState class rewritten for new UML diagram

--- src/engine/PlayState.java
+++ src/engine/PlayState.java
@@ -1,8 +1,9 @@
 package engine;
 
 import java.util.List;
+import java.awt.Point;
 
-import engine.enemies.Enemy;
+import engine.level.Level;
 import engine.managers.EnemyManager;
 import engine.managers.ProjectileManager;
 import engine.managers.TowerManager;
@@ -10,37 +11,61 @@ import engine.sprites.towers.Tower;
 import engine.sprites.towers.TowerI;
 
 /**
- * This class will hold all of the current Play information, such as the list of objects on the screen (enemies,
- * towers, projectiles, etc). Update method will be called every game loop to update the position of every object. 
- * @author ryanpond
- * @author Ben Hodgson 4/3/18
+ * 
  * @author Miles Todzo
  *
  */
 public class PlayState implements PlayStateI{
     
+	private int UNIVERSAL_TIME;
+	private int score;
+	private int money;
     private TowerManager myTowerManager;
     private EnemyManager myEnemyManager;
-    private ProjectileManager myProjectileManager;
-    private Level myLevel;
+    private EventHandler myEventHandler;
+    private List<Level> myLevels;
+    private Level currentLevel;
+    private boolean isPaused;
     
-	public PlayState() {
+	public PlayState(EventHandler eventHandler, List<Level> levels) {
+		myEventHandler = eventHandler;
+		myLevels = levels;
+		currentLevel = myLevels.get(0);
 		myTowerManager = new TowerManager();
 		myEnemyManager = new EnemyManager();
+		isPaused = false;
+		score = 0;
+		money = 0;
 	}
 
 	@Override
 	public void update() {
-		List<Enemy> enemyList = myEnemyManager.unmodifiableActiveList();
-		myTowerManager.checkForCollisions(enemyList);
-		myProjectileManager.chechForCollisions(enemyList);
+		UNIVERSAL_TIME++;
+		myTowerManager.checkForCollisions(myEnemyManager.getObservableListOfActive());
+		myEnemyManager.checkForCollisions(myTowerManager.getObservableListOfActive());
+		myTowerManager.moveProjectiles();
+		myTowerManager.moveTowers();
+		myEnemyManager.moveProjectiles();
+		myEnemyManager.moveEnemies();
+		currentLevel.getNewEnemy(UNIVERSAL_TIME, myEnemyManager);
 	}
 	
-	@Override
-	public void setLevel(Level newLevel) {
-		myLevel = level;
-		myTowerManager.setTowers(myLevel.getTowers()); //maybe change so that it adds on to the List and doesn't overwrite old towers
-		
+	public void setLevel(int levelNumber) {
+		currentLevel = myLevels.get(levelNumber);
+		myTowerManager.setTowers(currentLevel.getTowers()); //maybe change so that it adds on to the List and doesn't overwrite old towers
+		myEnemyManager.setEnemies(currentLevel.getEnemies());
+	}
+	
+	public void pause() {
+		isPaused = true;
+	}
+	
+	public void play() {
+		isPaused = false;
+	}
+	
+	public FrontEndTower placeTower(Point location, String towerType) {
+		return (FrontEndTower) myTowerManager.addTower(location, towerType);
 	}
 
 }
