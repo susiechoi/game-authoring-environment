355be1388f6563e9122aba3b32f2cb2551e04da2
Sarah Bland <scb49@duke.edu>
2018-04-04 23:51:32 -0400

    minor fix (language issue) in settingsscreen issue #11

--- src/authoring/frontend/SettingsScreen.java
+++ src/authoring/frontend/SettingsScreen.java
@@ -17,7 +17,7 @@ public class SettingsScreen extends AdjustScreen {
 	public static final String BACKGROUND_IMAGES = "images/BackgroundImageNames.properties";
 	public static final String DEFAULT_OWN_CSS = "styling/GameAuthoringStartScreen.css";
 	
-	protected SettingsScreen(AuthoringView view, String language) {
+	protected SettingsScreen(AuthoringView view) {
 		super(view);
 		setStyleSheet(DEFAULT_OWN_CSS);
 	}
