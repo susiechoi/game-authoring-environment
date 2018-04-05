8d318b3e9b1b6625dcafba03f028ec32ac49f200
Benjamin Auriemma <benjamin.auriemma@duke.edu>
2018-04-05 19:39:10 -0400

    deleted merge conflicts and Controller class which is not needed

--- src/engine/managers/TowerManager.java
+++ src/engine/managers/TowerManager.java
@@ -15,12 +15,8 @@ import engine.sprites.towers.Tower;
  * @author Katherine Van Dyk
  * @author Ryan Pond
  * @author Ben Hodgson 4/3/18
-<<<<<<< HEAD
  * @author Miles Todzo 4/4/18
- *
-=======
  * @author benauriemma 4/5
->>>>>>> 4ac2761c491e671aaf3cef6e3477aeb794eeffa5
  */
 public class TowerManager extends Manager {
 
