95e8c7e0c73561d38a8b6c532146debf66974f84
Sarah Bland <scb49@duke.edu>
2018-04-05 21:30:39 -0400

    issue #11 view interface

--- src/frontend/Screen.java
+++ src/frontend/Screen.java
@@ -1,12 +1,7 @@
 package frontend;
 import java.util.List;
 
-import authoring.frontend.AuthoringView;
-import authoring.frontend.exceptions.MissingPropertiesException;
-import frontend.*;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.Parent;
 
 /**
  * @author transition to abstract class & error checking - Sarah Bland
@@ -17,117 +12,84 @@ import javafx.scene.control.Alert.AlertType;
 
 public abstract class Screen {
 
-	public static final String DEFAULT_SHARED_STYLESHEET = "styling/SharedStyling.css";
-	public static final String DEFAULT_FILE_ERRORMESSAGE = "Missing specified language property files.";
-	public static final String DEFAULT_LANGUAGE = "English";
-	public static final String DEFAULT_PROMPT = "";
-	//private AuthoringView myView; 
-	private String myStylesheet; 
-	private Scene myScreen;
-	private UIFactory myUIFactory;
-	private PropertiesReader myPropertiesReader;
+    public static final String DEFAULT_SHARED_STYLESHEET = "styling/SharedStyling.css";
+    public static final String DEFAULT_FILE_ERRORMESSAGE = "Missing specified language property files.";
+    public static final String DEFAULT_LANGUAGE = "English";
+    public static final String DEFAULT_PROMPT = "";
+    //private AuthoringView myView; 
+    private String myStylesheet; 
+    private Parent myRoot;
+    private UIFactory myUIFactory;
+    private PropertiesReader myPropertiesReader;
 
-	protected Screen() {
-		myUIFactory = new UIFactory();
-		myPropertiesReader = new PropertiesReader();
-	}
-	
-	protected UIFactory getUIFactory() {
-		return myUIFactory;
-	}
-	
-	protected void setStyleSheet(String stylesheetString) {
-		myStylesheet = stylesheetString;
-	}
+    protected Screen() {
+	myUIFactory = new UIFactory();
+	myPropertiesReader = new PropertiesReader();
+    }
 
-	/**
-	 * Creates & returns the styled Screen
-	 */
-	protected Scene makeScreen() {
-		try {
-		myScreen = makeScreenWithoutStyling();
-		}
-		catch (MissingPropertiesException e){
-			getView().showErrorScreen("NoFile");
-		}
-		applyDefaultStyling();
-		applyStyle(myStylesheet);
-		return myScreen; 
-	}
+    protected UIFactory getUIFactory() {
+	return myUIFactory;
+    }
 
-	public abstract Scene makeScreenWithoutStyling() throws MissingPropertiesException;
+    protected void setStyleSheet(String stylesheetString) {
+	myStylesheet = stylesheetString;
+    }
 
-	public void applyDefaultStyling() {
-		if (myScreen != null) {
-			myScreen.getStylesheets().add(DEFAULT_SHARED_STYLESHEET);
-		}
-	}
+    /**
+     * Creates & returns the styled Screen
+     */
+    protected Parent makeScreen() {
+	myRoot = makeScreenWithoutStyling();
+	applyDefaultStyling();
+	applyStyle(myStylesheet);
+	return myRoot; 
+    }
 
-	public void applyStyle(String stylesheet) {
-		if (myScreen != null && stylesheet != null) {
-			myScreen.getStylesheets().add(stylesheet);
-		}
-	}
+    public abstract Parent makeScreenWithoutStyling();
 
-	public void applyStyles(List<String> stylesheets) {
-		if (myScreen != null) {
-			for (String s : stylesheets) {
-				myScreen.getStylesheets().add(s);
-			}		}
+    public void applyDefaultStyling() {
+	if (myRoot != null) {
+	    myRoot.getStylesheets().add(DEFAULT_SHARED_STYLESHEET);
 	}
+    }
 
-	/**
-	 * Returns the Scene object to be loaded on the screen
-	 */
-	public Scene getScreen() {
-		if (myScreen == null) {
-			myScreen = makeScreen(); 
-		}
-		return myScreen; 
+    public void applyStyle(String stylesheet) {
+	if (myRoot != null && stylesheet != null) {
+	    myRoot.getStylesheets().add(stylesheet);
 	}
+    }
 
-	public void setScreen(Scene newScreen) {
-		myScreen = newScreen;
-	}
-	
-	public void showError(String errorMessage) {
-		Alert errorAlert = new Alert(AlertType.ERROR, errorMessage);
-		errorAlert.showAndWait();
-	}
-	
-	
-	public PropertiesReader getPropertiesReader() {
-		return myPropertiesReader; 
-	}
+    public void applyStyles(List<String> stylesheets) {
+	if (myRoot != null) {
+	    for (String s : stylesheets) {
+		myRoot.getStylesheets().add(s);
+	    }		}
+    }
 
-	public String getErrorCheckedPrompt(String key, String language) {
-		String value = new String();
-		try {
-			value = myPropertiesReader.findVal(makePromptsFilepath(language), key);
-		}
-		catch(MissingPropertiesException e) {
-			try {
-				showError(myPropertiesReader.findVal(makeErrorsFilepath(language), "NoFile"));
-				value = myPropertiesReader.findVal(makePromptsFilepath(DEFAULT_LANGUAGE), key);
-			}
-			catch(MissingPropertiesException e2) {
-				showDefaultNoFilesError();
-				value = DEFAULT_PROMPT;
-			}
-		}
-		return value;
+    /**
+     * Returns the Scene object to be loaded on the screen
+     */
+    public Parent getScreen() {
+	if (myRoot == null) {
+	    myRoot = makeScreen(); 
 	}
-	
-	public void showDefaultNoFilesError() {
-		showError(DEFAULT_FILE_ERRORMESSAGE);
-	}
-	public String makeErrorsFilepath(String language) {
-		return "languages/"+language+"/Errors.properties";
-	}
-	public String makePromptsFilepath(String language) {
-		return "languages/"+language+"/Prompts.properties";
-	}
-	
+	return myRoot; 
+    }
+    public PropertiesReader getPropertiesReader() {
+	return myPropertiesReader; 
+    }
+
+
+    public String makeErrorsFilepath(String language) {
+	return "languages/"+language+"/Errors.properties";
+    }
+    public String makePromptsFilepath(String language) {
+	return "languages/"+language+"/Prompts.properties";
+    }
+    protected Parent getRoot() {
+	return myRoot;
+    }
+
 }
 
 
