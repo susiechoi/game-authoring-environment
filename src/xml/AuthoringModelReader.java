cac78c67eaaa162fedc7dabc4e0a1f1216aa1b9b
Brendan Cheng <bmc41@duke.edu>
2018-04-05 21:47:49 -0400

    Changed name of EngineCreator to AuthoringModelReader to better reflect class's purpose

--- src/xml/AuthoringModelReader.java
+++ src/xml/AuthoringModelReader.java
@@ -7,18 +7,19 @@ import com.thoughtworks.xstream.XStream;
 import authoring.AuthoringModel;
 
 /**
- * Reads an 
+ * Reads an XML document using XStream to reconstruct an AuthoringModel object to either be further edited by authoring environment or to create a game engine
+ * in the game play environment
  * 
  * @author Brendan Cheng
  *
  */
 
-public class EngineCreator implements XMLReader {
+public class AuthoringModelReader implements XMLReader {
 
 	private Document dom;
 	private XStream parser;
 	
-	public EngineCreator(String filename) {
+	public AuthoringModelReader(String filename) {
 		parser = new XStream();
 		dom = readInFile(filename);
 	}
@@ -27,7 +28,7 @@ public class EngineCreator implements XMLReader {
 	 * 
 	 * @return
 	 */
-	public AuthoringModel createEngine() {
+	public AuthoringModel createModel() {
 		return null;
 	}
 	
