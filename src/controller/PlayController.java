3a6b06ad3229e3814eab20a279efb08175845728
Ben Hodgson <bmh43@duke.edu>
2018-04-05 21:00:22 -0400

    Created PlayController class and implemented newPlay git add *

---
+++ src/controller/PlayController.java
@@ -0,0 +1,30 @@
+package controller;
+
+/**
+ * 
+ * @author Ben Hodgson 4/5/18
+ * 
+ * Created by the ChiefController and makes everything necessary to play a game. 
+ */
+public class PlayController {
+    
+    private Mediator myMediator;
+    private XMLReadery myXMLReader;
+    private ScreenManager myScreenManager;
+    private GameEngine myGameEngine;
+    
+    public PlayController() {
+	myMediator = new Mediator();
+	myScreenmanager = new ScreenManager();
+	myGameEngine = new GameEngine();
+	myMediator.setScreenManager(myScreenManager)
+	myMediator.setEngine(myGameEngine)
+    }
+    
+    public void newPlay(XMLFile file) {
+	AuthoringModel playModel = myXMLReader.read(file);
+	PlayState play = new PlayState(playModel.getLevels());
+	myGameEngine.setPlayState(play);
+	// TODO: myScreenManager.setLandscape(landscape);
+    }
+}
