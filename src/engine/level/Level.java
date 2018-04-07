422eff902835680dbc889e9be9252a54f42ddb2b
Ryan Pond <rp159@duke.edu>
2018-04-07 16:26:00 -0400

    fixing some enemy stuff, comments

--- src/engine/level/Level.java
+++ src/engine/level/Level.java
@@ -3,12 +3,14 @@ package engine.level;
 import java.util.ArrayList;
 import java.util.List;
 import engine.sprites.enemies.wave.Wave;
+import engine.sprites.towers.Tower;
 import engine.path.Path;
 
 /**
  * 
  * @author Ben Hodgson 3/29/18
- *
+ * @author Ryan Pond
+ * 
  * Interface for a game level
  */
 public class Level {
@@ -16,11 +18,13 @@ public class Level {
     private final List<Wave> WAVES;
     private final int NUMBER;
     private final Path PATH;
+    private List<Tower> availableTowers;
     
-    public Level(int number, Path path) {
+    public Level(int number, Path path, List<Tower> towersAvail) {
 	WAVES = new ArrayList<Wave>();
 	NUMBER = number;
 	PATH = path;
+	availableTowers = towersAvail;
     }
     
     
@@ -54,5 +58,14 @@ public class Level {
 	}
 	return true; 
     }
+
+    /**
+     * Returns the list of towers that are available to be placed on this level
+     * @return
+     */
+    public List<Tower> getTowers() {
+	return availableTowers;
+    }
+
 
 }
