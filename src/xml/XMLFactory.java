a41a167fc01c946206e3e46e14cfca695ecb3ee6
Brendan Cheng <bmc41@duke.edu>
2018-04-07 17:23:45 -0400

    Created working copy of XMLFactory. Uses reflection to build new XML readers/writers. Need to update printStackTraces. Issue #24, 25, 32

--- src/xml/XMLFactory.java
+++ src/xml/XMLFactory.java
@@ -1,7 +1,10 @@
 package xml;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
- * Static class that creates XMLWriters/XMLReaders to handle data parsing.
+ * Static class that creates XMLWriters/XMLReaders to handle data parsing. Holds a main method for testing purposes.
  *
  * @author Brendan Cheng
  *
@@ -14,14 +17,33 @@ public class XMLFactory {
 	 *
 	 * @param writerType
 	 * String representation of class name of desired writer type
+	 * 
+	 * @param filename
+	 * path to file that is being written to
 	 *
 	 * @return
 	 * Instance of that implementation of XMLWriter
 	 */
-	public static XMLWriter generateWriter(String writerType) {
+	public static XMLWriter generateWriter(String writerType, String filename) {
 		try {
 			Class writer = Class.forName(writerType);
-		} catch (ClassNotFoundException e) {
+			Constructor c = writer.getConstructor();
+			try {
+			    return (XMLWriter) c.newInstance(filename);
+			} catch (InstantiationException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (IllegalAccessException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (InvocationTargetException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			}
+		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -33,12 +55,41 @@ public class XMLFactory {
 	 *
 	 * @param readerType
 	 * String representation of class name of desired reader type
+	 * 
+	 * @param filename
+	 * Path to file tat is being read
 	 *
 	 * @return
 	 * Instance of that implementation of XMLReader
 	 */
-	public static XMLReader generateReader(String readerType) {
+	public static XMLReader generateReader(String readerType, String filename) {
+		try {
+			Class writer = Class.forName(readerType);
+			Constructor c = writer.getConstructor();
+			try {
+			    return (XMLReader) c.newInstance(filename);
+			} catch (InstantiationException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (IllegalAccessException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			} catch (InvocationTargetException e) {
+			    // TODO Auto-generated catch block
+			    e.printStackTrace();
+			}
+		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return null;
+	}
+	
+	public static void main(String[] args) {
+	    
 	}
 
 }
