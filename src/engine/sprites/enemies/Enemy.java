132dc99252495b548ab0cda63b2a8cfd7c8dbd6a
kmv10 <kmv10@duke.edu>
2018-04-08 16:12:44 -0400

    Added path object and put it into enemy

--- src/engine/sprites/enemies/Enemy.java
+++ src/engine/sprites/enemies/Enemy.java
@@ -1,5 +1,6 @@
 package engine.sprites.enemies;
 
+import engine.path.Path;
 import engine.physics.IIntersecter;
 import engine.physics.Intersecter;
 import engine.sprites.Sprite;
@@ -7,6 +8,7 @@ import engine.sprites.properties.DamageProperty;
 import engine.sprites.properties.HealthProperty;
 import engine.sprites.properties.ValueProperty;
 import engine.sprites.towers.projectiles.Projectile;
+import java.awt.geom.Point2D;
 import javafx.scene.Node;
 import javafx.scene.image.Image;
 
@@ -15,60 +17,64 @@ import javafx.scene.image.Image;
  * intersectable methods.
  * 
  * @author ryanpond
+ * @author Katherine Van Dyk
+ * @date 4/8/18
  *
  */
 public class Enemy extends Sprite{
 
-	private HealthProperty myHealth;
-	private DamageProperty myDamage;
-	private ValueProperty myValue;
-	private IIntersecter myIntersecter;
+    private HealthProperty myHealth;
+    private DamageProperty myDamage;
+    private ValueProperty myValue;
+    private IIntersecter myIntersecter;
+    private Path myPath;
+    private double mySpeed;
 
-	public Enemy(Image myImage, HealthProperty health, DamageProperty damage, ValueProperty value) {
-		super(myImage);
-		myIntersecter = new Intersecter(this.getImage()); 
-		myHealth = health;
-		myDamage = damage;
-		myValue = value;
-	}
+    public Enemy(Image myImage, HealthProperty health, DamageProperty damage, ValueProperty value, Path path) {
+	super(myImage);
+	myIntersecter = new Intersecter(this.getImage()); 
+	myHealth = health;
+	myDamage = damage;
+	myValue = value;
+	myPath = path;
+    }
 
-	/**
-	 * Tests to see if another ImageView overlaps with the Enemy
-	 * @param otherImage : other image (projectile, tower, etc)
-	 * @return boolean, yes or no
-	 */
-	public boolean overlap(Node otherImage) {
-		return myIntersecter.overlaps(otherImage); 
-	}
+    /**
+     * Tests to see if another ImageView overlaps with the Enemy
+     * @param otherImage : other image (projectile, tower, etc)
+     * @return boolean, yes or no
+     */
+    public boolean overlap(Node otherImage) {
+	return myIntersecter.overlaps(otherImage); 
+    }
 
-	/**
-         * Handles when the Enemy is hit by a tower
-         * 
-         * @param projectile: the projectile that hit the enemy
-         * @return : returns true if the enemy is still alive, false if it is dead
-         */
-	public boolean getHitBy(Projectile projectile) { // I don't think this is supposed to return a boolean -bma
-		myHealth.loseHealth(projectile.getDamage());
-		return myHealth.isAlive();
-	}
+    /**
+     * Handles when the Enemy is hit by a tower
+     * 
+     * @param projectile: the projectile that hit the enemy
+     * @return : returns true if the enemy is still alive, false if it is dead
+     */
+    public boolean getHitBy(Projectile projectile) { // I don't think this is supposed to return a boolean -bma
+	myHealth.loseHealth(projectile.getDamage());
+	return myHealth.isAlive();
+    }
 
-	/**
+    /**
      * Handles updating the enemy position to follow the path
      */
-	public void followPath() {
-		// TODO Auto-generated method stub
+    public void move(double elapsedTime) {
+	Point2D newPosition = myPath.nextPosition(elapsedTime, mySpeed);
+	myPath.nextPosition(elapsedTime, mySpeed);
+	this.getImage().setX(newPosition.getX());
+	this.getImage().setY(newPosition.getY());
+    }
 
-	}
-
-	/**
+    /**
      * Handles returning an enemy's damage after hitting a tower
      * 
      * @return Double: damage that Enemy incurs on the tower
      */
-	public Double damage() {
-		return myDamage.getProperty();
-	}
-
-
-
+    public Double damage() {
+	return myDamage.getProperty();
+    }
 }
