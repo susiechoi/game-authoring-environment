61c43710e061c0dfd77cf2474cb295e3822e8a30
Miles Todzo <mt265@duke.edu>
2018-04-08 22:01:53 -0400

    #45

--- src/engine/sprites/Sprite.java
+++ src/engine/sprites/Sprite.java
@@ -1,6 +1,7 @@
 package engine.sprites;
 
 import java.util.List;
+import java.util.Map;
 
 import javafx.collections.ObservableList;
 import javafx.scene.image.Image;
@@ -18,53 +19,41 @@ import javafx.scene.shape.Shape;
  */
 
 public class Sprite  {
-    private ImageView myImageView;
+	private ImageView myImageView;
 
 
-    /**
-     * Constructor that takes in a sprite's image
-     * Source for resizing image: https://stackoverflow.com/questions/27894945/how-do-i-resize-an-imageview-image-in-javafx
-     * 
-     * @param image: tower's initial image
-     * @param size: size of tower's image
-     */
-    public Sprite(Image image) {
-	myImageView = new ImageView(image);
-	myImageView.setPreserveRatio(true);
-    }
-    
-    /**
-     * Returns each Sprite's image
-     * 
-     * @return ImageView representing game object's image
-     */
-    public ImageView getImageView() { 
-	return myImageView;
-    }
-    
-    public void setImage(ImageView image) {
-	myImageView  = image;
-    }
-    
-    public void place(double newX, double newY) {
-	myImageView.setX(newX);
-	myImageView.setY(newY);
-    }
-    
-//    // TODO Should this method go in the sprite object? Need to specify that it is projectiles we're dealing with in order to get their damage
-//    public void checkForCollision(ShootingSprites shooter, ObservableList<Sprite> projectiles) {
-//    		shooter.checkTowerEnemyCollision((ShootingSprites) this); 
-//    		for (Sprite projectile: projectiles) {
-//    			ImageView spriteImageView = projectile.getImage();
-//    			if(this.myImageView.intersects(spriteImageView.getX(), spriteImageView.getY(), spriteImageView.getFitWidth(), spriteImageView.getFitHeight())){
-//    				this.handleCollision(projectile.getDamage());
-//    				projectile.handleCollision();
-//    			}
-//    		}
-//    }
+	/**
+	 * Constructor that takes in a sprite's image
+	 * Source for resizing image: https://stackoverflow.com/questions/27894945/how-do-i-resize-an-imageview-image-in-javafx
+	 * 
+	 * @param image: tower's initial image
+	 * @param size: size of tower's image
+	 */
+	public Sprite(Image image) {
+		myImageView = new ImageView(image);
+		myImageView.setPreserveRatio(true);
+	}
+
+	/**
+	 * Returns each Sprite's image
+	 * 
+	 * @return ImageView representing game object's image
+	 */
+	public ImageView getImageView() { 
+		return myImageView;
+	}
+
+	public void setImageView(ImageView image) {
+		myImageView  = image;
+	}
+
+	public void place(double newX, double newY) {
+		myImageView.setX(newX);
+		myImageView.setY(newY);
+	}
 
 	public void handleCollision() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
--- src/engine/sprites/towers/Tower.java
+++ src/engine/sprites/towers/Tower.java
@@ -1,5 +1,7 @@
 package engine.sprites.towers;
 
+import java.util.Map;
+
 import engine.sprites.ShootingSprites;
 import engine.sprites.properties.*;
 import engine.sprites.towers.launcher.Launcher;
@@ -12,70 +14,81 @@ import javafx.scene.image.Image;
  * @author Miles Todzo
  */
 public class Tower extends ShootingSprites {
-    private Launcher myLauncher;
-    private HealthProperty myHealth;
-    private ValueProperty myValue;
+	private Launcher myLauncher;
+	private HealthProperty myHealth;
+	private ValueProperty myValue;
+	private Map<String,  Double> propertyStats;
 
-    /**
-     * Constructor for a Tower object that accepts parameter properties.
-     * 
-     * @param image: Tower's image
-     * @param launcher: Type of launcher that the Tower inherits 
-     * @param health: Initial health of the tower
-     * @param value: Value of the tower for selling
-     */
-    public Tower(Image image, double size, Launcher launcher, HealthProperty health, ValueProperty value) {
-	super(image, size);
-	myLauncher = launcher;
-	myHealth = health;
-	myValue = value;
-    }
-    
-    /**
-     * Handles decrementing tower's damage when it gets hit by an enemy
-     * 
-     * @return boolean: True if tower is alive, false otherwise
-     */
-    public boolean handleCollision(double enemyDamage) {
-	myHealth.loseHealth(enemyDamage);
-	return (myHealth.getProperty() <= 0);
-    }
-    
-    /**
-     * Handles selling a tower
-     */
-    public double sell() {
-	return myValue.getProperty();
-    }
+	/**
+	 * Constructor for a Tower object that accepts parameter properties.
+	 * 
+	 * @param image: Tower's image
+	 * @param launcher: Type of launcher that the Tower inherits 
+	 * @param health: Initial health of the tower
+	 * @param value: Value of the tower for selling
+	 */
+	public Tower(Image image, double size, Launcher launcher, HealthProperty health, ValueProperty value) {
+		super(image, size);
+		myLauncher = launcher;
+		myHealth = health;
+		myValue = value;
+		propertyStats.put(health.getClass().getSimpleName(), health.getProperty());
+		propertyStats.put(value.getClass().getSimpleName(), value.getProperty());
+		propertyStats.put(this.getDamageProperty().getClass().getSimpleName(), this.getDamageProperty().getProperty());
+	}
 
-    /**
-     * Handles upgrading the health of a tower
-     */
-    public double upgradeHealth(double balance) {
-	return myHealth.upgrade(balance);
-    }
+	/**
+	 * Handles decrementing tower's damage when it gets hit by an enemy
+	 * 
+	 * @return boolean: True if tower is alive, false otherwise
+	 */
+	public boolean handleCollision(double enemyDamage) {
+		myHealth.loseHealth(enemyDamage);
+		return (myHealth.getProperty() <= 0);
+	}
 
-    /**
-     * Upgrades the rate of fire
-     */
-    public double upgradeRateOfFire(double balance) {
-	return myLauncher.upgradeFireRate(balance);
-    }
+	/**
+	 * Handles selling a tower
+	 */
+	public double sell() {
+		return myValue.getProperty();
+	}
 
-    /**
-     * Upgrades the amount of damage a tower's projectiles exhibit
-     */
-    public double upgradeDamage(double balance) {
-	return myLauncher.upgradeDamage(balance);
-    }
+	/**
+	 * Handles upgrading the health of a tower
+	 */
+	public double upgradeHealth(double balance) {
+		return myHealth.upgrade(balance);
+	}
 
-    /**
-     * Upgrades all aspects of a tower
-     */
-    public double upgrade(double balance) {
-	balance -= upgradeHealth(balance);
-	balance -= upgradeRateOfFire(balance);
-	return upgradeDamage(balance);
-    }
+	/**
+	 * Upgrades the rate of fire
+	 */
+	public double upgradeRateOfFire(double balance) {
+		return myLauncher.upgradeFireRate(balance);
+	}
 
+	/**
+	 * Upgrades the amount of damage a tower's projectiles exhibit
+	 */
+	public double upgradeDamage(double balance) {
+		return myLauncher.upgradeDamage(balance);
+	}
+
+	/**
+	 * Upgrades all aspects of a tower
+	 */
+	public double upgrade(double balance) {
+		balance -= upgradeHealth(balance);
+		balance -= upgradeRateOfFire(balance);
+		return upgradeDamage(balance);
+	}
+
+	public Map<String, Double> getTowerStats(){
+		return propertyStats;
+	}
+	
+	public DamageProperty getDamageProperty() {
+		return myLauncher.getDamageProperty();
+	}
 }
