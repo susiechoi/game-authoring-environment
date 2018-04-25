291f39ca030858aabe55ce2225eef1d22a0d3c8c
Susie Choi <sc477@duke.edu>
2018-04-24 23:17:19 -0400

    OPTION TO VIEW SCORE GRAPH OF LAST GAME PLAY IN MAINSCREEN #66

--- src/gameplayer/panel/ScorePanel.java
+++ src/gameplayer/panel/ScorePanel.java
@@ -1,19 +1,19 @@
 
 package gameplayer.panel;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import gameplayer.screen.GameScreen;
-import javafx.scene.layout.Priority;
-import javafx.stage.Stage;
-import javafx.scene.layout.HBox;
-import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 
 public class ScorePanel extends Panel {
 
-	private final String DEFAULT_SHARED_STYLESHEET = "styling/SharedStyling.css";
+	public static final String DEFAULT_DATAPOINTS_FILENAME = "graphing/scoregraph";
+	public static final String DEFAULT_SHARED_STYLESHEET = "styling/SharedStyling.css";
 
 	private final GameScreen GAME_SCREEN;
 	private Integer SCORE;
@@ -22,46 +22,34 @@ public class ScorePanel extends Panel {
 	private Label ScoreText;
 	private Label LevelText;
 	private Label HealthText;
-	
-	private NumberAxis myX; 
-	private NumberAxis myY;
-	private LineChart<Number, Number> myLineChart; 
-	private XYChart.Series mySeries; 
-	private int myXIncrement; 
-	
+
+	private PrintWriter myScoreWriter; 
+	private int myScoreXIncrement;
+
 	public ScorePanel(GameScreen gameScreen) {
 		GAME_SCREEN = gameScreen;
 		SCORE = 0;
 		HEALTH = 100;
 		LEVEL = 1;
-		
+
 		//TODO Read words SCORE, LEVEL, and + from properties file
 		ScoreText = new Label("Score: " + SCORE);
 		LevelText = new Label("Level " + LEVEL);
 		HealthText = new Label("+" + HEALTH);
-		
-		setupScoreGraphing();
-	}
-	
-	private void setupScoreGraphing() {
-		Stage graphingStage = new Stage(); 
-		myX = new NumberAxis();
-		myY = new NumberAxis(); 
-		myLineChart = new LineChart<Number, Number>(myX, myY);
-		mySeries = new XYChart.Series<>(); 
-		myLineChart.getData().add(mySeries); 
-		Scene graphingScene = new Scene(myLineChart, 800, 600);
-		graphingStage.setScene(graphingScene);
-		graphingStage.show();
-	}
 
+		try {
+			myScoreWriter = new PrintWriter(new FileWriter(DEFAULT_DATAPOINTS_FILENAME), true);
+		} catch (IOException e) {
+			GAME_SCREEN.loadErrorScreen("NoFile");
+		}
+	}
 
 	@Override
 	public void makePanel() {
 		ScoreText.setMaxWidth(Double.MAX_VALUE);
 		LevelText.setMaxWidth(Double.MAX_VALUE);
 		HealthText.setMaxWidth(Double.MAX_VALUE);
-		
+
 		HBox panelRoot = new HBox();
 
 		HBox.setHgrow(ScoreText, Priority.ALWAYS);
@@ -76,13 +64,16 @@ public class ScorePanel extends Panel {
 	}
 
 	public void updateScore(Integer newScore) {
-		myXIncrement++; 
-		mySeries.getData().add(new XYChart.Data(myXIncrement, newScore));
+		myScoreXIncrement++; 
+
+		myScoreWriter.write(Integer.toString(myScoreXIncrement)+" ");
+		myScoreWriter.write(Integer.toString(newScore)+"\n");
+
 		ScoreText.setText("Score: " + newScore);
 	}
 
-	public void updateHealth(double  myHealth) {
-		HealthText.setText("+" + Double.valueOf(Math.floor(myHealth)).intValue());
+	public void updateHealth(double newHealth) {
+		HealthText.setText("+" + Double.valueOf(Math.floor(newHealth)).intValue());
 	}
 
 	public void updateLevel(Integer newLevel) {
