778045a980e477f19e44f380e4b76a3ec03547d2
Susie Choi <sc477@duke.edu>
2018-04-28 01:02:34 -0400

    fixed most of screen flow in graphing #66

--- src/authoring/frontend/GraphMenuScreen.java
+++ src/authoring/frontend/GraphMenuScreen.java
@@ -53,12 +53,12 @@ public class GraphMenuScreen extends AuthoringScreen {
 				e -> {singleButton.setDisable(true);}, choosePrompt);
 		singleButton.setDisable(true);
 		singleButton.setOnAction(e -> {
-			ArrayList<String> args = new ArrayList<>(); 
+//			ArrayList<String> args = new ArrayList<>(); 
 			String fullFilepath  = makeFullFilepath(singleChooser.getSelectionModel().getSelectedItem()); 
-			args.add(fullFilepath);
-			args.add(myGraphType);
-//			getView().getStageManager().switchScreen(new SingleGraphScreen(getView(), fullFilepath, myGraphType).getScreen());
-			getView().goForwardFrom(this.getClass().getSimpleName()+"Single", args);
+//			args.add(fullFilepath);
+//			args.add(myGraphType);
+			getView().getStageManager().switchScreen(new SingleGraphScreen(getView(), fullFilepath, myGraphType).getScreen());
+//			getView().goForwardFrom(this.getClass().getSimpleName()+"Single", args);
 		});
 		
 		vb.getChildren().addAll(singleChooser, singleButton);
@@ -76,14 +76,14 @@ public class GraphMenuScreen extends AuthoringScreen {
 			e -> {compareButton.setDisable(true);}, choosePrompt);
 		compareButton.setDisable(true);
 		compareButton.setOnAction(e -> {
-			ArrayList<String> args = new ArrayList<>(); 
+//			ArrayList<String> args = new ArrayList<>(); 
 			String game1Path = makeFullFilepath(game1Chooser.getSelectionModel().getSelectedItem()); 
 			String game2Path = makeFullFilepath(game2Chooser.getSelectionModel().getSelectedItem()); 
-			args.add(game1Path);
-			args.add(game2Path);
-			args.add(myGraphType);
-//			getView().getStageManager().switchScreen(new DoubleGraphScreen(getView(), game1Path, game2Path, myGraphType).getScreen());
-			getView().goForwardFrom(this.getClass().getSimpleName()+"Double", args);
+//			args.add(game1Path);
+//			args.add(game2Path);
+//			args.add(myGraphType);
+			getView().getStageManager().switchScreen(new DoubleGraphScreen(getView(), game1Path, game2Path, myGraphType).getScreen());
+//			getView().goForwardFrom(this.getClass().getSimpleName()+"Double", args);
 		});
 
 		vb.getChildren().addAll(game1Chooser, game2Chooser, compareButton);
