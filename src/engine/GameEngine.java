dde4332dc006ee2195c9804d634bd50622dda9ec
Alexi Kontos <aak29@duke.edu>
2018-04-29 15:36:18 -0400

    changes

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -1,5 +1,6 @@
 package engine;
 
+import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
@@ -10,112 +11,125 @@ import xml.XMLFactory;
  * This class will handle all of the gameLoop interactions, and will also hold the 
  * current instance of the PlayState
  * @author ryanpond
- *
+ * 
  */
 public class GameEngine {
 
-    private final int FRAMES_PER_SECOND = 60;
-    private final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
-    private final Integer DEFAULT_RELATIVE_SPEED = 5;
-    private final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
-    private PlayState myPlayState;
-    private Mediator myMediator;
-    private Timeline ANIMATION;
+	private final int FRAMES_PER_SECOND = 60;
+	private final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+	private final Integer DEFAULT_RELATIVE_SPEED = 5;
+	private final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+	private PlayState myPlayState;
+	private Mediator myMediator;
+	private Timeline ANIMATION;
+	private double timeFactor;
+	private boolean gameTriggered = false;
 
-    public GameEngine(Mediator mediator) {
-	System.out.println("in engine");
-	myPlayState = null;
-	myMediator = mediator;
+	public GameEngine(Mediator mediator) {
+		myPlayState = null;
+		myMediator = mediator;
+		timeFactor = 1;
 
-	setSpeed(DEFAULT_RELATIVE_SPEED);
-	// attach "game loop" to time line to play it
-        KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
-                                      e -> loop(SECOND_DELAY));
-        ANIMATION = new Timeline();
-        ANIMATION.setCycleCount(Timeline.INDEFINITE);
-        ANIMATION.getKeyFrames().add(frame);
-    }
+		setSpeed(DEFAULT_RELATIVE_SPEED);
+		// attach "game loop" to time line to play it
+		KeyFrame frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY),
+				e -> loop(SECOND_DELAY));
+		ANIMATION = new Timeline();
+		ANIMATION.setCycleCount(Animation.INDEFINITE);
+		ANIMATION.getKeyFrames().add(frame);
 
-    public void setPlayState(PlayState p) {
-	myPlayState = p;
-    }
+	}
 
-    public PlayState getPlayState() {
-	return myPlayState;
-    }
-    
+	public void setPlayState(PlayState p) {
+		myPlayState = p;
+		
+	}
 
-    /**
-     * Calls the update function every loop
-     * @param elapsedTime
-     */
-    public void loop(double elapsedTime) {
-	myPlayState.update(elapsedTime);
-    }
+	public PlayState getPlayState() {
+		return myPlayState;
+	}
 
 
-    /**
-     * Pauses Game Loop animation so Game State stays constant
-     * COMMENTED OUT BECAUSE THIS WOULD PAUSE THE ENTIRE GAMEENGINE NOT THE PLAYSTATE
-     */
-//    public void pause() {
-//	ANIMATION.pause();
-//
-//    }
+	/**
+	 * Calls the update function every loop
+	 * @param elapsedTime
+	 */
+	public void loop(double elapsedTime) {
+		myPlayState.update(elapsedTime*timeFactor);
+	}
 
-    /**
-     * Starts Game Loop animation, so Game State continuously loops
-     */
-    public void start() {
-	ANIMATION.play();
 
-    }
+	/**
+	 * Pauses Game Loop animation so Game State stays constant
+	 * COMMENTED OUT BECAUSE THIS WOULD PAUSE THE ENTIRE GAMEENGINE NOT THE PLAYSTATE
+	 */
+	 public void pause() {
+	     ANIMATION.pause();
+	
+	 }
 
-    /**
-     * Sets Game Loop speed, to determine how fast level steps through.
-     * 
-     * @param speed: speed at which animation should iterate
-     * (relative speed of program, 1 being slowest, 5 being normal, 10 being fastest)
-     */
-    public void setSpeed(Integer relativeSpeed) {
-	Integer speed = MILLISECOND_DELAY*(relativeSpeed/DEFAULT_RELATIVE_SPEED);
-	//System.out.println("Incomplete setSpeed method, make sure to check this functionality!");
-	//TODO myTimer.schedule may need bugfixing for scaling speed
-    }
+	/**
+	 * Starts Game Loop animation, so Game State continuously loops
+	 */
+	public void start() {
+		myMediator.gameStarted();
+		while (gameTriggered) {
+			ANIMATION.play();
+		}
 
-    /**
-     * Saves current Game State to File
-     */
-    public void savePlay(String filename) {
-	PlaySaverWriter p = (PlaySaverWriter) XMLFactory.generateWriter("PlaySaverWriter");
-	p.write(myPlayState, filename);
-    }
+	}
 
-    /**
-     * Updates Game State to new Level as specified in XML File
-     * 
-     * @param l: integer denoting level to jump to
-     */
-    public void jumpLevel(int l) {
+	/**
+	 * Sets Game Loop speed, to determine how fast level steps through.
+	 * 
+	 * @param speed: speed at which animation should iterate
+	 * (relative speed of program, 1 being slowest, 5 being normal, 10 being fastest)
+	 */
+	public void setSpeed(Integer relativeSpeed) {
+		timeFactor = relativeSpeed;
+	}
 
-    }
+	/**
+	 * Saves current Game State to File
+	 */
+	public void savePlay(String filename) {
+		PlaySaverWriter p = (PlaySaverWriter) XMLFactory.generateWriter("PlaySaverWriter");
+		p.write(myPlayState, filename);
+	}
 
-    /**
-     * Jumps to a certain level played in the Play
-     * 
-     * @param newLevel : The PlayState object with information about that level
-     */
-    public void setLevel(PlayState newLevel) {
+	/**
+	 * Updates Game State to new Level as specified in XML File
+	 * 
+	 * @param l: integer denoting level to jump to
+	 */
+	public void jumpLevel(int l) {
+		//TODO: 
+	}
 
-    }
+	/**
+	 * Jumps to a certain level played in the Play
+	 * 
+	 * @param newLevel : The PlayState object with information about that level
+	 */
+	public void setLevel(PlayState newLevel) {
 
-    /**
-     * Restarts the current level
-     */
-    public void restartLevel() {
+	}
+	
 
-    }
+	/**
+	 * Stops game loop for when user wants to return to editing/authoring the game
+	 * @author susiechoi
+	 */
+	public void endLoop() {
+		if (ANIMATION != null) {
+			ANIMATION.stop();
+		}
+	}
 
-   
+
+	public void triggerGame() {
+		gameTriggered = true;
+	}
+
 
 }
