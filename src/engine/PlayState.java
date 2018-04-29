e2cbaee6d85d02bf69b91d5094b666ace5a07ff5
Alexi Kontos <aak29@duke.edu>
2018-04-29 15:40:09 -0400

    I hate git

--- src/engine/PlayState.java
+++ src/engine/PlayState.java
@@ -1,53 +1,45 @@
+
 package engine;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import authoring.frontend.exceptions.MissingPropertiesException;
 import data.GameData;
 
 import java.awt.Point;
-import java.io.FileNotFoundException;
-
 import engine.level.Level;
 import engine.managers.EnemyManager;
 import engine.managers.TowerManager;
 import engine.path.Path;
 import engine.sprites.enemies.Enemy;
 import engine.sprites.enemies.wave.Wave;
-import engine.sprites.ShootingSprites;
+import engine.sprites.FrontEndSprite;
 import engine.sprites.towers.CannotAffordException;
 import engine.sprites.Sprite;
 import engine.sprites.towers.FrontEndTower;
+import engine.sprites.towers.Tower;
 import engine.sprites.towers.projectiles.Projectile;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.scene.input.KeyCode;
 
-
+//TODO add in money to the game
 /**
  * Handles the current state of the game, including current score, money, and lists
  * of active towers and enemies
  * @author Miles Todzo
  * @author Katherine Van Dyk
  * @author benauriemma 4/8
- * @author Ben Hodgson 
- * @author Ryan Pond
+ * @date 4/6/18
  */
 public class PlayState implements GameData {
 
-    private int count;
-    private IntegerProperty myScore;
-    private IntegerProperty myResources;
-    private IntegerProperty myHealth;
-    private Settings mySettings;
+    private double UNIVERSAL_TIME;
+    private int myScore;
+    private int myResources;
     private TowerManager myTowerManager;
     private EnemyManager myEnemyManager;
     private Mediator myMediator;
     private List<Level> myLevels;
     private Level currentLevel;
-    private int currlvl;
-    private boolean backgroundSet;
+    private boolean isPaused;
 
     /**
      * Constructor for play state object that sets up initial levels.
@@ -58,146 +50,74 @@ public class PlayState implements GameData {
      * @param resources
      * @param universalTime
      */
-    public PlayState(Mediator mediator, List<Level> levels, int score, Settings settings, double universalTime) {
+    public PlayState(Mediator mediator, List<Level> levels, int score, int resources, double universalTime) {
 	myMediator = mediator;
 	myLevels = levels;
-	currlvl = 0;
 	currentLevel = myLevels.get(0);
 	myTowerManager = new TowerManager(currentLevel.getTowers());
 	myEnemyManager = new EnemyManager();
-	myScore = new SimpleIntegerProperty(score);
-	myResources = new SimpleIntegerProperty((int) settings.startingMoney());
-	myHealth = new SimpleIntegerProperty((int) settings.startingHealth());
-	myMediator.addIntegerProperties(myResources, myScore, myHealth);
-	mySettings=settings;
+	isPaused = false;
+	myScore = score;
+	myResources = resources;
+	UNIVERSAL_TIME = universalTime;
 	List<FrontEndTower> availTowers = new ArrayList<>();
 	availTowers.addAll(currentLevel.getTowers().values());
+	System.out.print("Available towers: ");
+	System.out.println(availTowers.size());
 	myMediator.setAvailableTowers(availTowers);
 	myTowerManager.setAvailableTowers(currentLevel.getTowers().values());
-	count = 0;
-	backgroundSet = false;
+	myEnemyManager.addToActiveList(new Enemy("Ryan", "images/robot.png", 100));
+
     }
 
     public void update(double elapsedTime) {
-	//Background has to be passed after a layout pass has been done on the Scene in order to adapt to
-	//differences in computers screen size 
-	if(!backgroundSet) {
-	    backgroundSet = myMediator.setPath(myLevels.get(0).getLevelPathMap(), myLevels.get(0).getBackGroundImage(), myLevels.get(0).getPathSize(), myLevels.get(0).getGridWidth(), myLevels.get(0).getGridHeight());
-	}
-	count++;
-	checkLoss();
-	if (count % 120 == 0) {
-	    spawnEnemies();
-	}
-	List<Sprite> deadEnemies = myEnemyManager.moveEnemies(elapsedTime);
-	updateHealth(deadEnemies);
-	myMediator.removeListOfSpritesFromScreen(deadEnemies);
-	handleCollisions(elapsedTime);
-    }
-
-    private void handleCollisions(double elapsedTime) {
-	List<Sprite> toBeRemoved = new ArrayList<>();
-	toBeRemoved.addAll(myTowerManager.checkForCollisions(myEnemyManager.getListOfActive()));
-	List<ShootingSprites> activeEnemies = myEnemyManager.getListOfActive();
-	List<ShootingSprites> activeTowers = myTowerManager.getListOfActive();
-	activeEnemies.removeAll(toBeRemoved);
-	activeTowers.removeAll(toBeRemoved);
-	myEnemyManager.removeFromMap(toBeRemoved);
-	myEnemyManager.setActiveList(activeEnemies);
-	//toBeRemoved.addAll(myEnemyManager.checkForCollisions(myTowerManager.getListOfActive()));
-	//myTowerManager.moveProjectiles(elapsedTime);
-	//toBeRemoved.addAll(myTowerManager.moveProjectiles(elapsedTime));
-	toBeRemoved.addAll(myTowerManager.moveProjectiles(elapsedTime));
-
-	for (Projectile projectile: myTowerManager.shoot(myEnemyManager.getListOfActive(), elapsedTime)) {
-	    myMediator.addSpriteToScreen(projectile);
+	if(!isPaused) {
 	    try {
-		myMediator.getSoundFactory().playSoundEffect("cannon"); // THIS SHOULD BE CUSTOMIZED: should be something like playSoundEffect(projectile.getSound())
-	    } catch (FileNotFoundException e) {
-		e.printStackTrace(); // YIKES THaT'S AND EASY FAIL
-	    }
-	}
-	updateScore(toBeRemoved);
-	myMediator.removeListOfSpritesFromScreen(toBeRemoved);
-    }
-
-
-    private void spawnEnemies() {
-
-	try {
-	    if (currentLevel.getWave(0).isFinished()) {
-		currentLevel.removeWave();   
-	    }
-	    Wave currentWave = currentLevel.getWave(0);
-	    for (Path currentPath : currentLevel.getPaths()) {
-		try {
-		    Enemy newEnemy = currentWave.getEnemySpecificPath(currentPath);
-		    newEnemy.setInitialPoint(currentPath.initialPoint());
-		    //newEnemy.updateImage();
-		    //enemy.move(path.initialPoint(),elapsedTime);
-		    myEnemyManager.addEnemy(currentPath, newEnemy);
-		    myEnemyManager.addToActiveList(newEnemy);
-		    myMediator.addSpriteToScreen(newEnemy);
+		for (Path path : currentLevel.getUnmodifiablePaths()) {
+		    Wave currentWave;
+		    if (!currentLevel.getWaves(path).isEmpty()) {
+			currentWave = currentLevel.getWaves(path).get(0);
+		    }
+		    else {
+			continue;
+		    }
+		    int currentTime = new Double(UNIVERSAL_TIME).intValue();
+		    if (UNIVERSAL_TIME == currentTime && !currentWave.isFinished()) {
+			currentLevel.getNewEnemy(path);
+		    }
+		    // TODO: remove "magic numbers", improve this to be 3 seconds
+		    // after the wave finished
+		    if (UNIVERSAL_TIME % 3 == 0 && currentWave.isFinished()) {
+			currentLevel.removeWave(path);
+		    }
 		}
-		catch (Exception e) {
-		    // do nothing, path contains no enemies TODO this seems like e.printstacktrace? not trying to die
-		}
+	    } catch (Exception e) {
+		// do nothing
 	    }
-	}
-	catch (Exception e) {
-	    // Level is over
-	    if (currentLevel.isFinished() && currentLevel.myNumber() < myLevels.size()) {
-		currentLevel = myLevels.get(currentLevel.myNumber());
-		myMediator.updateLevel(currentLevel.myNumber());
-		// TODO: call Mediator to trigger next level
-		myMediator.nextLevel();
-		try {
-		    myMediator.getSoundFactory().playSoundEffect("traphorn"); // I DONT KNOW IF THIS ONE WORKS
-		} catch (FileNotFoundException e1) {
-		    e1.printStackTrace(); //TODO!!!
-		}
-		
+	    UNIVERSAL_TIME+=elapsedTime;
+	    List<Sprite> toBeRemoved = new ArrayList<>();
+	    //toBeRemoved.addAll(myTowerManager.checkForCollisions(myEnemyManager.getListOfActive()));
+	    //toBeRemoved.addAll(myEnemyManager.checkForCollisions(myTowerManager.getListOfActive()));
+	    myTowerManager.moveProjectiles(elapsedTime);
+	    myTowerManager.moveTowers();
+	    for (Projectile projectile: myTowerManager.shoot(myEnemyManager.getListOfActive())) {
+		myMediator.addSpriteToScreen((FrontEndSprite)projectile);
+		System.out.println("added to med");
 	    }
-	    else {
-		// TODO: end game, player won
-			myMediator.gameWon();
-	    }
-	}
-    }
-    
-    private void checkLoss() {
-	if (myHealth.getValue() <= 0) {
-	    System.out.println("Lost game!");
-	    myMediator.pause();
-	    myMediator.endLoop();
-	    myMediator.gameLost();
-	    try {
-		myMediator.getSoundFactory().playSoundEffect("boo"); // ALSO SHOULD BE CUSTOMIZED
-	    } catch (FileNotFoundException e) {
-		e.printStackTrace(); // TODO: 
-	    }
+//	    for (Projectile projectile: myEnemyManager.shoot(myTowerManager.getListOfActive())) {
+//		myMediator.addSpriteToScreen((FrontEndSprite)projectile);
+//	    }
+//	    myEnemyManager.moveProjectiles();
+//	    myEnemyManager.moveEnemies();
+	    myMediator.removeListOfSpritesFromScreen(toBeRemoved);
 	}
     }
 
-    private void updateScore(List<Sprite> toBeRemoved) {
-	for(Sprite sprite : toBeRemoved) {
-	    myScore.set(myScore.get() + sprite.getPointValue());
-	    if(sprite.getClass().getName().equals("engine.sprites.enemies.Enemy")) {
-		Enemy enemy = (Enemy) sprite;
-		myResources.set(myResources.get() + enemy.getPointValue());
-	    }
-	}
-    }
-    
-    private void updateHealth(List<Sprite> toBeRemoved) {
-	for(Sprite sprite : toBeRemoved) {
-	    if(sprite.getClass().getName().equals("engine.sprites.enemies.Enemy")) {
-		Enemy enemy = (Enemy) sprite;
-		myHealth.set(myHealth.get() 
-			- Double.valueOf(Math.round(enemy.getDamage())).intValue());
-	    }
-	}
-    }
+
+
+    //    public void upgradeTower(FrontEndTower tower, String upgradeName) throws CannotAffordException {
+    //	myResources -= tower.upgrade(upgradeName);
+    //    }
 
     public void setLevel(int levelNumber) {
 	currentLevel = myLevels.get(levelNumber);
@@ -205,72 +125,37 @@ public class PlayState implements GameData {
 	myEnemyManager.setEnemies(currentLevel.getEnemies().values());
     }
 
-    /**
-     * Restarts the level that you were currently on.
-     */
     public void restartLevel() {
-	clearLevel();
-	setLevel(currlvl);
+	// TODO Auto-generated method stub
     }
 
-    private void clearLevel() {
-	List<Sprite> toBeRemoved = new ArrayList<>();
-	toBeRemoved.addAll(myTowerManager.getListOfActive());
-	toBeRemoved.addAll(myTowerManager.removeAllProjectiles());
-	toBeRemoved.addAll(myEnemyManager.getListOfActive());
-	myTowerManager.getListOfActive().clear();
-	myEnemyManager.getListOfActive().clear();
+    public void pause() {
+	isPaused = true;
     }
 
-    /**
-     * Places a tower, adds to the necessary backend data structures
-     * @param location : The point on the map where the tower is added
-     * @param towerType : Type of tower
-     * @return : the front end tower
-     * @throws CannotAffordException : thrown if the user does not have enough money
-     */
+    public void play() {
+	isPaused = false;
+    }
+
     public FrontEndTower placeTower(Point location, String towerType) throws CannotAffordException {
 	FrontEndTower placedTower = myTowerManager.place(location, towerType);
-	try {
-	    myResources.set(placedTower.purchase(myResources.get()));
-	    return placedTower;
-	}
-	catch(CannotAffordException e) {
-	    myTowerManager.sell(placedTower);
-	    throw new CannotAffordException(e.getMessage());
-	}
+	//	myResources = placedTower.purchase(myResources);
+	//	myMediator.updateCurrency(myResources);
+	return placedTower;
     }
+
+    //    public void upgradeTower(FrontEndTower tower, String upgradeName) throws CannotAffordException {
+    //	myResources -= tower.upgrade(upgradeName);
+    //    }
+
     /**
      * Sells the tower, increments users currency, and removes it from collection and screen
      * @param tower
      */
     public void sellTower(FrontEndTower tower) {
-	myTowerManager.upgrade(tower,"rando",myResources.get());
-	myResources.set(myResources.get()+myTowerManager.sell(tower));
-	myMediator.removeSpriteFromScreen(tower);
-	try {
-	    myMediator.getSoundFactory().playSoundEffect("cash"); //TODO: make custom
-	} catch (FileNotFoundException e) {
-	    e.printStackTrace(); //TODO
-	} 
-    }
-
-    /**
-     * Called when a certain tower is to be upgraded. The type of upgrade is specified
-     * with the string upgradeName
-     * @param tower
-     * @param upgradeName
-     */
-    public void upgradeTower(FrontEndTower tower, String upgradeName) {
-	myResources.set((int) myTowerManager.upgrade(tower,upgradeName,myResources.get())); 
-    }
-    
-    public String getStyling() throws MissingPropertiesException {
-    	return mySettings.getCSSTheme();
-    }
-
-    public void moveTowers(FrontEndTower tower, KeyCode c) {
-    	System.out.println("PLAY STATE");
-	myTowerManager.moveTowers(tower, c);
+	myResources += myTowerManager.sell(tower);
+	myMediator.updateCurrency(myResources);
+	myMediator.removeSpriteFromScreen((FrontEndSprite)tower);
     }
 }
+
