4415bd7ab3205371b3273521974b4346ea1e9d5c
Ben Hodgson <bmh43@duke.edu>
2018-04-28 23:20:10 -0400

    Deleting master, preparing for full scale revert and revolt

--- src/authoring/frontend/exceptions/DeleteDefaultException.java
+++
@@ -1,9 +0,0 @@
-package authoring.frontend.exceptions;
-
-public class DeleteDefaultException extends Exception{
-
-    public DeleteDefaultException(String message) {
-	super(message);
-    }
-
-}
--- src/controller/MVController.java
+++
@@ -1,8 +0,0 @@
-package controller;
-
-import authoring.frontend.exceptions.MissingPropertiesException;
-import frontend.StageManager;
-
-public interface MVController {
-    public void playControllerDemo(StageManager manager, String instructions) throws MissingPropertiesException;
-}
